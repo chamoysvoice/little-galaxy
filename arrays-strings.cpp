a855446b9e7b25de143cfdfaaa2e6997267648b3
Alejandro G. Leal <chamoysvoice@gmail.com>
2015-09-13 17:59:08 -0500

    added description of the problem

--- arrays-strings.cpp
+++ arrays-strings.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 #include<string>
+
+/*
+ * Problem source: "Cracking the Coding Interview" - 5ed - page: 73
+ * Implement an algorithm to determine if a string has all unique characters. (1st part)
+ * What if you cannot use additional data structures? (2nd part)
+ */
 bool isUnique(std::string s){
 	unsigned short int frequency[256] = {0};
 	for(int i = 0; i < s.length(); i++){
