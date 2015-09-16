53b003a937ddb762759526ff2b609e8aa9734f42
Jesus Garza <chamoysvoice@gmail.com>
2015-09-16 15:07:47 -0500

    organize according the book

---
+++ CTCI/arrays-strings.h
@@ -0,0 +1,6 @@
+#ifndef ARRAYS_STRINGS_H
+#define ARRAYS_STRINGS_H
+bool isUnique(std::string s);
+bool isUniqueWOArray(std::string s);
+char *reverseString(char* string);
+#endif
---
+++ CTCI/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "arrays-strings.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	/* problem 1 */
+	/*
+	string s = "is not unique";
+	string s2 = "uniqe yop";
+	
+	cout << isUnique(s2);
+	cout << isUniqueWOArray(s);
+	*/
+	/* problem 2 */
+	char s[10] = "123456789";
+
+	cout << reverseString(s);
+}
