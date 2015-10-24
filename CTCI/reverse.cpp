c95400503b9555aeef62105f134b70d2350879cd
Jesus Garza <chamoysvoice@gmail.com>
2015-10-23 20:29:15 -0500

    reverse string in place

--- CTCI/reverse.cpp
+++ CTCI/reverse.cpp
@@ -7,14 +7,30 @@ using namespace std;
 void reverseString(char* s, char* r){
 	int length = strlen(s);
 	int j = 0;
-	for(int i = length - 1; i >=0; i--){ 
+	for(int i = length - 1; i >=0; i--){
 		r[j] = s[i];
-		j++;	
+		j++;
+	}
+}
+
+void reverseInPlace(char *str){
+	char *end = str;
+	char t;
+	// go back for last char
+	while(*end != '\0'){
+		end++;
+	}
+	end--;
+
+	while(end > str){
+		t = *end;
+		*end-- = *str;
+		*str++ = t;
 	}
 }
 
 int main(){
-    char s[SIZE] = "123456789", r[SIZE];
-    reverseString(s, r);
-    cout << "this is the value of r " << r;
+    char s[SIZE] = "123456789";
+    reverseInPlace(s);
+    cout << "this is the value of s " << s;
 }
